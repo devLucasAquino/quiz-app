fde9c23032c1cb0c12e01b8c47386549dfe8e1b8
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-07-07 21:59:00 -0300

    feat: gerenciando estado Quiz component

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,9 @@ function App() {
     return(
         <>
             <Header />
-            <Quiz />
+            <main>
+                <Quiz />
+            </main>
         </>
     )
 }
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,11 +1,28 @@
 import { useState } from "react";
 
+import QUESTIONS from '../questions.js';
+
 export default function Quiz(){
-    const [ activeQuestionIndex, seActiveQuestionIndex ] = useState(0);
     const [ userAnswer, setUserAnswer ] = useState([]);
 
+    const activeQuestionIndex = userAnswer.length;
 
+    function handleSelectAnswer(selectedAnswer){
+        setUserAnswer((prevUserAnswers) => {
+            return [ ...prevUserAnswers, selectedAnswer ]
+        });
+    }
     return(
-        <p>Currenty active question</p>
+        <div id="quiz">      
+            <div id="question">
+                <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+                <ul id="answers">
+                    {QUESTIONS[activeQuestionIndex].answers.map((answer) => 
+                    <li key={answer} className="answer">
+                        <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
+                    </li>)}
+                </ul>
+            </div>
+        </div>
     )
 }
