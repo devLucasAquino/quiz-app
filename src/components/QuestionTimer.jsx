9b48a220f455225d399a54758ffd91ab8afdbcb6
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-07-08 16:51:55 -0300

    fix: limpando interval e timeout

--- src/components/QuestionTimer.jsx
+++ src/components/QuestionTimer.jsx
@@ -5,13 +5,21 @@ const QuestionTimer = ({ timeout, onTimeout }) => {
     const [ remainingTime, setRemainingTime ] = useState(timeout);
 
     useEffect(() => {
-        setTimeout(onTimeout, timeout);
+        const timer = setTimeout(onTimeout, timeout);
+
+        return () => {
+            clearTimeout(timer);
+        };
     }, [ timeout, onTimeout ]);
 
     useEffect(() => {
-        setInterval(() => {
+        const interval = setInterval(() => {
             setRemainingTime(prevRemainingTime => prevRemainingTime - 100);
         }, 100)
+
+        return () => {
+            clearInterval(interval);
+        };
     }, []);
 
 
